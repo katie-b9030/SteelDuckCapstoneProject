57e050c971b6d809758acf6b6762337a432ef2e1
Kristin Yap <144386730+kmyap128@users.noreply.github.com>
2025-09-29 16:47:06 -0400

    added experimental arduino -> js comm file

---
+++ src/arduino.js
@@ -0,0 +1,13 @@
+        const { SerialPort } = require('serialport');
+        const { ReadlineParser } = require('@serialport/parser-readline');
+
+        const port = new SerialPort({ path: 'COM5', baudRate: 9600 }); 
+        const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
+
+        port.on('open', () => console.log('Serial port open'));
+
+        parser.on('data', data => {
+          console.log('Arduino output:', data);
+        });
+
+        port.on('error', err => console.error('Serial port error:', err.message));
