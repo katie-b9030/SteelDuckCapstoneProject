5bf749de12f3d13dc17535fe872b61db3dac108a
Tommy Seternus <tas3788@g.rit.edu>
2025-10-05 14:32:57 -0400

    updated html and canvas

--- src/canvas.js
+++ src/canvas.js
@@ -1,9 +1,14 @@
-function setup() {
-    createCanvas(600, 400);
+"use strict";
+import * as arduino from './arduino.js'
 
-    background('black');
-    fill('red');
-    square(100, 200, 100);
-    fill('yellow');
-    circle(400, 200, 100, 0);
+
+window.setup = function () {
+  createCanvas(600, 400);
+  background('black');
+
+  fill('red');
+  square(100, 200, 100);
+
+  fill('yellow');
+  circle(400, 200, 100);
 }
