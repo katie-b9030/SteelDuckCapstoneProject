a2cc1964a401de817a44a0743cfe521d0f17cb5f
Tommy Seternus <tas3788@g.rit.edu>
2025-10-07 15:14:34 -0400

    got movement and scaling working for both shapes

--- client/canvas.js
+++ client/canvas.js
@@ -1,14 +1,67 @@
 "use strict";
-import * as arduino from './arduino.js'
+//import * as arduino from './arduino.js'
+
+let squareX = 100;
+let squareY = 200;
+let circleX = 400;
+let circleY = 200;
+let circleD = 100;
+let squareA = 100;
+
+function squareMove() { // Square is the Barrel Rotary Encoder
+
+    if (keyIsDown(RIGHT_ARROW)) { // WILL BECOME BarrelRotation = Clockwise
+        squareX += 5;
+    } else if (keyIsDown(LEFT_ARROW)) { // WILL BECOME BarrelRotation = CounterClockwise
+        squareX -= 5;
+    }
+}
+
+function circleMove() { // Circle is the Barrel Powerup Rotary Encoder
+
+    if (keyIsDown(49)) { // (49 = 1 key) WILL BECOME BarrelPowerup = 1st value
+        circleY = 50;
+    } else if (keyIsDown(50)) { // (50 = 2 key) WILL BECOME BarrelPowerup = 2nd value
+        circleY = 150;
+    } else if (keyIsDown(51)) { // (51 = 3 key) WILL BECOME BarrelPowerup = 3rd value
+        circleY = 250;
+    }
+}
+
+function squareIncrease() {
+    if (keyIsDown(84)) { // (84 is T KEY) WILL BECOME BarrelPressed
+        squareA += 1;
+    } else if (keyIsDown(89)) { // 89 is Y KEY
+        squareA -= 1;
+    }
+}
+
+
+function circleIncrease() {
+    if (keyIsDown(32)) { // (32 is spacebar) WILL BECOME BarrelPoweupPressed
+        circleD += 1;
+    } else if (keyIsDown(16)) { // 16 is shift
+        circleD -= 1;
+    }
+}
 
 
 window.setup = function () {
-  createCanvas(600, 400);
-  background('black');
-
-  fill('red');
-  square(100, 200, 100);
-
-  fill('yellow');
-  circle(400, 200, 100);
+    createCanvas(600, 400);
 }
+
+window.draw = function () {
+    background('black');
+
+    squareMove();
+    circleMove();
+    circleIncrease();
+    squareIncrease();
+
+    fill('red');
+    square(squareX, squareY, squareA);
+
+    fill('yellow');
+    circle(circleX, circleY, circleD);
+}
+
