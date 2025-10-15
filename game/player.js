9c845ec35cfa36ed9aae9561a82be1144fedc1cc
Kristin Yap <kmyap128@gmail.com>
2025-10-15 14:18:28 -0400

    started creating player class

---
+++ game/player.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor(team, role) {
+        this.team = team;
+        this.role = role;
+
+        this.state = "waiting";
+        this.taskComplete = false;
+    }
+
+    update() {
+        
+    }
+}
