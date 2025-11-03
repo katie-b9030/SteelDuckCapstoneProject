3f00ff1082cd5433007abd9e147e43d35a101633
Kristin Yap <kmyap128@gmail.com>
2025-11-03 14:52:55 -0500

    removed unnecessary commented code

--- client/src/barrelCanvasArduino.js
+++ client/src/barrelCanvasArduino.js
@@ -1,19 +1,4 @@
-//TODO: get this working with arduino
-
 "use strict";
-
-// const socket = io();
-
-// let barrelData = {};
-// let cannonData = {};
-
-// socket.on("barrelData", (data) => {
-//   barrelData = data;
-// });
-
-// socket.on("cannonData", (data) => {
-//   cannonData = data;
-// });
 
 import { ArduinoController } from "./ArduinoController.js";
 
@@ -49,7 +34,7 @@ let squareColor = "#2355ddff";
 let triColor = "#2355ddff";
 
 window.preload = function () {
-  progressBar = loadImage("../media/assets/ui/Bubble_Bar_Empty.png"); // path to your image
+  progressBar = loadImage("../media/assets/ui/Bubble_Bar_Empty.png"); 
   // barrel_img = loadImage("../media/assets/ui/barrel.png");
 };
 
