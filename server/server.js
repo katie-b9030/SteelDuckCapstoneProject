ed847f5122d0fc5084b2b9284041d6f4394e5d09
katie-b9030 <klb9030@rit.edu>
2025-11-06 15:01:29 -0500

    updading for errors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const EXPRESS = require("express");
 const HTTP = require("http");
-const { io } = require("socket.io");
+const { Server } = require("socket.io");
 const ARDUINO_PARSER = require("./arduino");
 
 const APP = EXPRESS();
@@ -12,8 +12,8 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 APP.use(EXPRESS.static("client"));
 APP.use("/media", EXPRESS.static("media"));
 
-ARDUINO_PARSER.subscribe("barrel", (data) => SOCKET.emit("barrelData", data));
-ARDUINO_PARSER.subscribe("cannon", (data) => SOCKET.emit("cannonData", data));
+ARDUINO_PARSER.subscribe("barrel", (data) => Server.emit("barrelData", data));
+ARDUINO_PARSER.subscribe("cannon", (data) => Server.emit("cannonData", data));
 
 SERVER.listen(port, () => {
   console.log(`Listening on 127.0.0.1: ${port}`);
