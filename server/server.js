9e86677c39bc63dcbbbd81913246e0b2d21d61b7
katie-b9030 <klb9030@rit.edu>
2025-11-06 15:02:08 -0500

    another server bug update

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 APP.use(EXPRESS.static("client"));
 APP.use("/media", EXPRESS.static("media"));
 
-ARDUINO_PARSER.subscribe("barrel", (data) => Server.emit("barrelData", data));
-ARDUINO_PARSER.subscribe("cannon", (data) => Server.emit("cannonData", data));
+ARDUINO_PARSER.subscribe("barrel", (data) => io.emit("barrelData", data));
+ARDUINO_PARSER.subscribe("cannon", (data) => io.emit("cannonData", data));
 
 SERVER.listen(port, () => {
   console.log(`Listening on 127.0.0.1: ${port}`);
