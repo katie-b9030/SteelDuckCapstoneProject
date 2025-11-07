8304f854e3fbe5723ea197bb4343e93e2d700567
Kristin Yap <144386730+kmyap128@users.noreply.github.com>
2025-11-07 12:40:06 -0500

    Update server.js trying to allow canvas file to access live updates

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const EXPRESS = require("express");
 const HTTP = require("http");
 const { Server } = require("socket.io");
 const ARDUINO_PARSER = require("./arduino");
+const { startGameLoop } = require("../game/gameManager.js");
 
 const APP = EXPRESS();
 const SERVER = HTTP.createServer(APP);
@@ -11,9 +12,12 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 APP.use(EXPRESS.static("client"));
 APP.use("/media", EXPRESS.static("media"));
+APP.use("/game", EXPRESS.static("game"));
 
 ARDUINO_PARSER.subscribe("barrel", (data) => io.emit("barrelData", data));
 ARDUINO_PARSER.subscribe("cannon", (data) => io.emit("cannonData", data));
+
+startGameLoop(io);
 
 SERVER.listen(port, () => {
   console.log(`Listening on 127.0.0.1: ${port}`);
