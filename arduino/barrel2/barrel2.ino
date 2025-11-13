0beb576188053383419761d15e8283599acbe9ce
Kristin Yap <144386730+kmyap128@users.noreply.github.com>
2025-11-13 15:26:32 -0500

    Add files via upload

---
+++ arduino/barrel2/barrel2.ino
@@ -0,0 +1,101 @@
+const int MAGNETIC_PIN = 9;  
+const int POWERUP_BTN = 10;    // Arcade button
+const char* POWERUPS[] = {
+  "shield",
+  "chest",
+  "helmet"
+};
+const int MAX_SPINS = 10;
+
+
+//Vars
+int spinCount = 0;
+bool magnetDetected = false;
+bool powerupSelected = false;
+int i = 0;
+String selectedPowerup = POWERUPS[i];
+
+
+void setup() {
+  Serial.begin(9600);
+
+  //magnet sensor setup
+  pinMode(MAGNETIC_PIN, INPUT_PULLUP);
+
+  // arcade button setup
+  pinMode(POWERUP_BTN, INPUT_PULLUP);
+
+
+}
+
+void loop() {
+  if(powerupSelected == false) {
+    Serial.print("Powerup | ");
+    spinCount = 0;
+    Serial.print(spinCount);
+    Serial.print(" | ");
+
+    int state = digitalRead(MAGNETIC_PIN);
+
+    if (state == LOW && !magnetDetected) {
+      magnetDetected = true;
+      i = (i == 2) ? 0 : i + 1;
+    }
+    else if (state == HIGH && magnetDetected) {
+      magnetDetected = false;
+    }
+
+    selectedPowerup = POWERUPS[i];
+
+    Serial.print(selectedPowerup);
+    Serial.print(" | ");
+
+    if (digitalRead(POWERUP_BTN) == LOW) {
+      Serial.println("Button Pressed");
+      powerupSelected = true;
+    } else {
+      Serial.println("Button Released");
+    }
+  }
+  else {
+    Serial.print("Spin | ");
+
+    //magnet spin
+    int state = digitalRead(MAGNETIC_PIN);
+
+    if (state == LOW && !magnetDetected) {
+      magnetDetected = true;
+      spinCount++;
+    }
+    else if (state == HIGH && magnetDetected) {
+      magnetDetected = false;
+    }
+    
+    Serial.print(spinCount);
+    Serial.print(" | ");
+
+    Serial.print(selectedPowerup);
+    Serial.print( " | ");  
+
+    if(spinCount >= MAX_SPINS) spinCount = MAX_SPINS;
+
+    //arcade button press
+    if (digitalRead(POWERUP_BTN) == LOW && spinCount == MAX_SPINS) {
+      Serial.println("Button Pressed");
+      powerupSelected = false;
+      i = 0;
+      delay(500);
+    } else {
+      Serial.println("Button Released");
+    }
+  }
+
+
+
+
+
+  
+
+
+  
+}
